e726be43e3bb40fce9e40b0b2d38fa647fc14fc8
David Sawatske <sawatskeda10@gmail.com>
2017-07-06 20:59:15 -0700

    Import redux-promise and added AJAX request useing axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 const API_KEY = 'ff16622aca1226a8209e8837e8690b86';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: types.FETCH_WEATHER,
+    payload: request
+  };
+}
