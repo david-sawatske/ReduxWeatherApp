e735255cf0e40b09d84444d58069ade9721e7461
David Sawatske <sawatskeda10@gmail.com>
2017-07-07 19:17:02 -0700

    Fix state bug by adding return of state in weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 export default function(state = [], action) {
   switch (action.type) {
-  case types.FETCH_WEATHER:
+  case FETCH_WEATHER:
     return [action.payload.data, ...state];
   }
+  
   return state;
 }
