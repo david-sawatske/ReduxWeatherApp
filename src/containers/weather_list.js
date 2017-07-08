954df91ac82e2da0920e28020e59dbc148fc4140
David Sawatske <sawatskeda10@gmail.com>
2017-07-07 19:27:25 -0700

    Add rows for searched cities in list container

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const cityName = cityData.city.name;
+    
+    return (
+      <tr key={cityName}>
+        <td>{cityName}</td>
+      </tr>
+    )
+  }
+
   render() {
     return(
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temp</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
