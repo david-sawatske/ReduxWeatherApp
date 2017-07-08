179abcee781dcd026a81816d8baf7e7f0734bd35
David Sawatske <sawatskeda10@gmail.com>
2017-07-07 17:57:33 -0700

    Add FETCH_WEATHER export to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const API_KEY = 'ff16622aca1226a8209e8837e8690b86';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
